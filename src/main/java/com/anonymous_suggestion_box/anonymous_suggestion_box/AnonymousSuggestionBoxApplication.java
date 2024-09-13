3c713e8f597a3cfcc28e0df891655d835dd64168
Mariana Martins <marih_ms@hotmail.com>
2024-09-13 19:28:46 -0300

    first commit

---
+++ src/main/java/com/anonymous_suggestion_box/anonymous_suggestion_box/AnonymousSuggestionBoxApplication.java
@@ -0,0 +1,13 @@
+package com.anonymous_suggestion_box.anonymous_suggestion_box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousSuggestionBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonymousSuggestionBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anonymous_suggestion_box/anonymous_suggestion_box/AnonymousSuggestionBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anonymous_suggestion_box.anonymous_suggestion_box;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymousSuggestionBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
